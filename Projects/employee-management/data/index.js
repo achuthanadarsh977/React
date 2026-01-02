82eb71b1a5b87a3490e0addc2ef999bdadd85b90
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2026-01-02 19:21:29 +0530

    Create index.js

---
+++ Projects/employee-management/data/index.js
@@ -0,0 +1,45 @@
+
+const employeedata = [
+
+    {
+        id:1,
+        name:"Susan Winters",
+        gender:"Female",
+        bloodgroup:"B+",
+        email:"susanwinters@gmail.com",
+        salary:'80000',
+        date:"2024-01-01"
+    },
+
+    {
+        id:2,
+        name:"Daniel Shah",
+        gender:"Male",
+        bloodgroup:"C+",
+        email:"danielshah@gmail.com",
+        salary:"40000",
+        date:"2025-06-06"
+    },
+
+    {
+        id:3,
+        name:"Don Johnson",
+        gender:"Male",
+        bloodgroup:"D+",
+        email:"donjohnson@gmail.com",
+        salary:"50000",
+        date:"2023-03-03"
+    },
+
+    {
+        id:4,
+        name:"Sarah Connor",
+        gender:"Female",
+        bloodgroup:"J+",
+        email:"johnmilton@gmail.com",
+        salary:"60000",
+        date:"2020-04-04"
+    }
+]
+export default employeedata
+export default employeedata
