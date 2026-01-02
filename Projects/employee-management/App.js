03ba1270279adc49f3ef57c46ff1e0232a0e6c9a
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2026-01-02 19:23:27 +0530

    Add files via upload

---
+++ Projects/employee-management/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DashBoard from "./Page/Dashboard"
+
+function App() {
+  return (
+    <div>
+     <DashBoard />
+    </div>
+  );
+}
+
+export default App;
