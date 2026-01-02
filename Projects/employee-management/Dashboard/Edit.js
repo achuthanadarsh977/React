7c9af60aa6f761847149922bb27a9be2b7e53d37
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2026-01-02 19:22:49 +0530

    Add files via upload

---
+++ Projects/employee-management/Dashboard/Edit.js
@@ -0,0 +1,15 @@
+
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
+
+
+function Edit(employees , selectedEmployee , setEmployees , setIsEditing){
+
+    const id = selectedEmployee.id
+
+    
+
+}
+
+
+export default Edit
