6bf5f518072fd9cefee0239f6c10b729ab3a67f1
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2026-01-05 19:50:33 +0530

    Add files via upload

---
+++ Projects/logistics-management-system/Add.js
@@ -0,0 +1,22 @@
+import React , {useState , useRef , useEffect} from "react"
+
+import Swal from "sweetalert2"
+
+function Add({logistics , setlogistics , setIsAdding}){
+
+        const [id , setid] = useState('');
+        const [supplierName , setsupplierName]  = useState('');
+        const [supplierPan , setsupplierPan] = useState('');
+        const [tripid , settripid] = useState('');
+        const [tripDate , settripDate] = useState('');
+        const [billingTo , setbillingto] = useState('');
+        const [fromLocation , setfromLocation] = useState('');
+        const [toLocation , settoLocation] = useState('');
+        const [loadingDate,setloadingDate] = useState('');
+        const [vehicleNumber , setvehicleNumber] = useState('');
+        const [] = useState('')
+
+     
+
+      
+}
---
+++ Projects/logistics-management-system/DashBoard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import List from "./List";
+
+const DashBoard = () => {
+  return (
+    <div className="container">
+      <List />
+    </div>
+  );
+};
+
+export default DashBoard;
